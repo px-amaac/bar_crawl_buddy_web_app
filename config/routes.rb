005c852d3702aee61c1e1529fee412fcdf7523ab
DoubleAA <px.amaac@gmail.com>
2013-02-20 00:14:15 -0800

    commit for the night 2/19/13 about 4 hours in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 BarCrawlBuddy::Application.routes.draw do
-
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
+  get "static_pages/home"
+  get "static_pages/about"
 
 
   # The priority is based upon order of creation:
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,32 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Bar Crawl Buddy'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Bar Crawl Buddy')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                    :text => "Bar Crawl Buddy | Home")
+  	end
   end
+
+  describe "About page" do
+
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Bar Crawl Buddy | About")
+    end
+end
 end
