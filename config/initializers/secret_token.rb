2b55032d4e256e3c5f89c49967080d1018509f94
DoubleAA <px.amaac@gmail.com>
2013-02-19 21:38:43 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarCrawlBuddy::Application.config.secret_token = '95a7552d2408777d692767c6547496f6aa6fdefa6c74ed2e41440e89b5b5df88cca35c009d8b707a32063e0d6e7baf3237b4a070753b6fb929294593db279cb8'
