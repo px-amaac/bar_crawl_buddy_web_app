456fe2872567d609d16a424264eaebcd4f5f553f
DoubleAA <px.amaac@gmail.com>
2013-02-26 14:47:01 -0800

    Finished Froint Page Start

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,10 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
+
+  def help
+  end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,8 @@
+def full_title(page_title)
+	base_title = "Bar Crawl Buddy"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
