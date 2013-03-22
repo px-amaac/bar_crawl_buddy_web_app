7b0e8e0f62b7db1d675708f3d061d2c9441f9076
DoubleAA <px.amaac@gmail.com>
2013-03-21 17:05:29 -0700

    add bar attribute validations and rspec tests

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -12,4 +12,8 @@
 
 class Bar < ActiveRecord::Base
   attr_accessible :bar_name, :lat, :long
+
+  validates :bar_name, presence: true, length: { maximum: 50 }
+  validates :lat, presence: true
+  validates :long, presence: true
 end
--- spec/models/bar_spec.rb
+++ spec/models/bar_spec.rb
@@ -13,5 +13,34 @@
 require 'spec_helper'
 
 describe Bar do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+  	@bar = Bar.new( bar_name: "banshee", lat: 123213.23, long: 123321.21)
+  end
+  subject { @bar }
+
+  it { should respond_to(:bar_name) }
+  it { should respond_to(:lat) }
+  it { should respond_to(:long) }
+  it { should be_valid }
+
+  describe "when bar name is not present" do
+  	before { @bar.bar_name = " " }
+  	it { should_not be_valid }
+  end
+  describe "when bar name is too long" do
+  	before { @bar.bar_name = "a" * 51 }
+  	it { should_not be_valid }
+  end
+
+  describe "when lat is not present" do
+  	before { @bar.lat = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when long is not present" do
+  	before { @bar.long = " " }
+  	it { should_not be_valid }
+  end
+
 end
