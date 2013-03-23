60f921ba03062dc1e0e01cd6db53d9f03300f6e4
DoubleAA <px.amaac@gmail.com>
2013-03-23 00:56:12 -0700

    final commit for the night 3/23 not working

--- app/controllers/manager_relationships_controller.rb
+++ app/controllers/manager_relationships_controller.rb
@@ -3,10 +3,15 @@ class ManagerRelationshipsController < ApplicationController
 
 	def create
 		@bar = Bar.find(params[:manager_relationship][:bar_id])
-		current_user.manage!(@bar)
+		if @bar.authenticate(params[:manager_relationship][:password])
+			current_user.manage!(@bar)
 		respond_to do |format|
 			format.html { redirect_to @bar }
 			format.js
+		end
+		else
+			flash.now[:error] = 'Invalid password'
+			render 'new'
 		end
 	end
 	def destroy
